030177f4a498121483b20483376e9e51a650f15c
Carlos Oliveira <bhjcarlos@hotmail.com>
2017-12-04 12:15:02 -0200

    Método de lista

--- class/usuario.php
+++ class/usuario.php
@@ -58,6 +58,45 @@ class Usuario {
 		}
 	}
 
+	public static function getList() { //funçao quando não usa o this pode ser usado um método static 
+			// com isso não precisa instanciar o método em index
+		$sql = new Sql();
+
+		return $sql -> select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	public static function search($login) {
+
+		$sql = new Sql();
+
+		return $sql -> select ("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH' => "%" . $login . "%"
+
+		));
+
+
+	}
+
+	public function login($login, $password) {
+
+			$sql = new Sql();
+
+		$results = $sql -> select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN" => $login,
+			":PASSWORD" => $password
+		));
+
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}	
+	}
+
 	public function __toString() {
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -3,15 +3,26 @@
 require_once("config.php");
 
 //$sql = new Sql();
-
 //$usuarios = $sql -> select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//função carrega apenas um usuario
+//$root = new Usuario();
+//$root -> loadbyId(5);
+//echo $root;
 
-$root -> loadbyId(5);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuario buscando pelo login
+//$search = Usuario::search("us");
+//echo json_encode($search);
+
+//Carrega um usuario usando um login e a senha
+$usuario = new Usuario();
+$usuario -> login("root", "909090");
+
+echo $usuario;
 
 ?>
